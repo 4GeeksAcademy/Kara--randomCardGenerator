477c3aac39faceebc74e31073182c7bab5664243
Karai2mil <128420458+Karai2mil@users.noreply.github.com>
2023-05-27 05:10:13 +0000

    subimos todo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numbersOfCards = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let randomNumber =
+  numbersOfCards[Math.floor(Math.random() * numbersOfCards.length)];
+let randomNumberElement = document.getElementById("randomNumber");
+randomNumberElement.innerHTML = randomNumber;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let suits = [
+  { suit: "♦", color: "red" },
+  { suit: "♥", color: "red" },
+  { suit: "♠", color: "black" },
+  { suit: "♣", color: "black" }
+];
+let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+let randomSuitElement = document.querySelectorAll(".randomSuit");
+
+randomSuitElement.forEach(element => {
+  element.innerHTML = randomSuit.suit;
+  element.style.color = randomSuit.color;
+  randomNumberElement.style.color = randomSuit.color;
+});
+
+document.getElementById("button").addEventListener("click", function() {
+  location.reload();
+});
